134d6ad55156c608b7752e99af146483e4d77c20
Andrey Nikonorov <nknrw@yandex.ru>
2022-10-16 20:37:06 +0300

    fix 7

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ app.post('/signin', celebrate({
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().required().regex(/^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
